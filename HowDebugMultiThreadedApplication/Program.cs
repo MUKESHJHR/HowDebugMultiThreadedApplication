366c0c87fc1a7b1884814fdc5a927cf933d80159
mukesh kumar <mukesh.1006jhr@outlook.com>
2023-08-12 19:59:30 +0530

    Add project files.

---
+++ HowDebugMultiThreadedApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HowDebugMultiThreadedApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(SomeMethod)
+            {
+                Name="Thread 1"
+            };
+
+            Thread t2 = new Thread(SomeMethod)
+            {
+                Name="Thread 2"
+            };
+
+            t1.Start();
+            t2.Start();
+
+            Console.ReadKey();
+        }
+
+        public static void SomeMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                i++;
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
